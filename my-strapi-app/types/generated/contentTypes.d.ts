3747bcf61ae1163206ad16df0fddc5774e1a4011
sagarpandey141 <121431268+sagarpandey141@users.noreply.github.com>
2024-07-08 12:59:44 +0530

    ss

--- my-strapi-app/types/generated/contentTypes.d.ts
+++ my-strapi-app/types/generated/contentTypes.d.ts
@@ -788,6 +788,58 @@ export interface PluginI18NLocale extends Schema.CollectionType {
   };
 }
 
+export interface ApiExampleExample extends Schema.CollectionType {
+  collectionName: 'examples';
+  info: {
+    singularName: 'example';
+    pluralName: 'examples';
+    displayName: 'example';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    ss: Attribute.JSON;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::example.example',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::example.example',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
+export interface ApiNewNew extends Schema.CollectionType {
+  collectionName: 'news';
+  info: {
+    singularName: 'new';
+    pluralName: 'news';
+    displayName: 'new';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    sss: Attribute.JSON;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<'api::new.new', 'oneToOne', 'admin::user'> &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<'api::new.new', 'oneToOne', 'admin::user'> &
+      Attribute.Private;
+  };
+}
+
 declare module '@strapi/types' {
   export module Shared {
     export interface ContentTypes {
@@ -806,6 +858,8 @@ declare module '@strapi/types' {
       'plugin::users-permissions.role': PluginUsersPermissionsRole;
       'plugin::users-permissions.user': PluginUsersPermissionsUser;
       'plugin::i18n.locale': PluginI18NLocale;
+      'api::example.example': ApiExampleExample;
+      'api::new.new': ApiNewNew;
     }
   }
 }
